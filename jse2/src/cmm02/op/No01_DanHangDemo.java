6257ce0292f16ed2f45ab8848b3e08665c820c19
Administrator <Administrator@MSDN-SPECIAL>
2015-05-12 11:52:49 +0900

    ..

---
+++ jse2/src/cmm02/op/No01_DanHangDemo.java
@@ -0,0 +1,19 @@
+package cmm02.op;
+
+public class No01_DanHangDemo {
+	public static void main(String[] args) {
+		//int i; 변수의 선언
+		//i = 5; 초기화, 할당	
+		int i = 5;
+		i = i + 1;
+		System.out.println("i = i + 5의값: " + i);
+		
+		int j = 5;
+		j++;
+		System.out.println("j++ 의값"+j);
+		
+		int k = 5;
+		++k;
+		System.out.println("k++의 값" + k);
+	}
+}
