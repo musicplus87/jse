22d22100f384ebfe3775fa73ebe917e078504192
Administrator <Administrator@MSDN-SPECIAL>
2015-05-13 11:56:16 +0900

    ..

---
+++ jse2/src/cmm02/op/No05_ConditionDemo.java
@@ -0,0 +1,16 @@
+package cmm02.op;
+
+public class No05_ConditionDemo {
+	public static void main(String[] args) {
+		int val01 = 1;
+		int val02 = 2;
+		if((val01 == 1) && (val02 == 2)){
+			System.out.println("val01 의 값은 1이고, val02 의 값은 2이다.");
+			
+		}
+		if((val01 == 1) || (val02==1)){
+			System.out.println("val01 이 1이거나, val02가 1이다.");
+		}
+	}
+
+}
