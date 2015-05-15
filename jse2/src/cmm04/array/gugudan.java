2da9551706c529985569cd93da843eeb2d1a620d
Administrator <Administrator@MSDN-SPECIAL>
2015-05-15 11:49:06 +0900

    ..

---
+++ jse2/src/cmm04/array/gugudan.java
@@ -0,0 +1,23 @@
+package cmm04.array;
+
+public class gugudan {
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			for (int j = 2; j < 6; j++) {
+		
+				System.out.print(j + "*" + i + "=" + j*i + "  ");
+			}
+			    System.out.println("");
+		}
+		        System.out.println("");
+		
+		for (int i = 1; i < 10; i++) {
+			for (int j = 6; j < 10; j++) {
+		
+				System.out.print(j + "*" + i + "=" + j*i + "  ");
+			}
+			    System.out.println("");
+		}
+		
+	}
+}
