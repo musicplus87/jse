fe118e38d604ebe24e00a343daff29c53a0acec0
Administrator <Administrator@MSDN-SPECIAL>
2015-05-29 11:55:04 +0900

    ..

---
+++ jse2/src/Api02/util/hashMap/KeySetDemo.java
@@ -0,0 +1,30 @@
+package Api02.util.hashMap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class KeySetDemo {
+	public static void main(String[] args) {
+		Map map = new HashMap();
+		//new Integer(98)는 숫자타입이 아닌 객체98임
+		
+		map.put("김연아", new Integer(98));
+		map.put("아사다 마오", new Integer(60));
+		map.put("소트니", new Integer(30));
+		
+		Set set = map.entrySet();
+		Iterator it = set.iterator();
+		
+		while(it.hasNext()){
+			//it.next()를 코딩하면 에러가
+			//발생하는데 Entry로 캐스팅 하면 된다.
+			Map.Entry e = (Entry) it.next();
+			System.out.println("참가자:"+e.getKey()
+					+", 점수:"+e.getValue());
+		}
+	}
+
+}
---
+++ jse2/src/Api02/util/vector/AddDemo.java
@@ -0,0 +1,48 @@
+package Api02.util.vector;
+
+import java.util.Vector;
+/*
+ * java.util.Vector
+ * -컬렉션 프레임웍에서 가장 많이 사용되는 컬렉션 클래스
+ * -객체만 저장할 수 있으며, 기본데이터타입은 저장 불가능
+ * -배열의 단점인 메모리 낭비를 피할 수 있는 구조
+ * -Thread에 안정적인 컬렉션 클래스
+ * -동적 크기 변경이 가능
+ */
+
+public class AddDemo {
+	public static void main(String[] args) {
+		Vector<String> vec = new Vector<String>();
+		String admin = new String("admin");
+		Boolean gender = new Boolean(false);
+		
+		vec.add(new String("password"));
+		vec.add(String.valueOf(23));
+		vec.add(String.valueOf(gender));
+		vec.add(admin);
+		
+		System.out.println("벡터에 담긴 내용 출력");
+		/*
+		 * for loop 에서 limit 부분을 보시면
+		 * 배열에서 쓰는 length()가 아니라
+		 * size()를 쓴 것에 주의!!!
+		 */
+		/*
+		 * 배열이 인덱스가 0부터 시작하는 것처럼
+		 * 벡터도 인덱스가 0부터 시작합니다.
+		 * 
+		 */
+		for(int i=0;i<vec.size();i++){
+			System.out.println("벡터에 들어있는"
+					+(i+1)+"번째 요소는"
+					+vec.elementAt(i)
+					+"입니다.");
+		}
+		/*
+		 * 출력시키는 메소드도 elementAt()
+		 * 인것에 주의!!
+		 * 배열과 다릅니다.
+		 */
+	}
+
+}
